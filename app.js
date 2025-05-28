eb168a97970759593275998340d868faed1e8aab
yeshichoden08 <yesheyychoden909@gmail.com>
2025-05-28 11:08:40 +0600

    app.js

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ app.use(session({
 
 // View engine
 app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, 'Views'));
 
 // Routes
 app.use('/', authRoutes);
